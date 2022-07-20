2c5209cb2c23c5833a4382263e73ef5a2a5cd561
Dylan Hays <twentygx@outlook.com>
2022-07-19 22:08:34 -0500

    commit tree 4d8a77b2055e43132b42ab3776f97ea5da86ec67

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+const findMatching = (drivers, name) => drivers.filter(d => d.toLowerCase() === name.toLowerCase());
+
+const fuzzyMatch = (drivers, begin) => drivers.filter(d => d.startsWith(begin));
+
+const matchName = (drivers, name) => drivers.filter(d => d.name === name);
